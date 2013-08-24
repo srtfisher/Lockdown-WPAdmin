d9b54ff8005627a5c24ff13c81721c2084d31306
Sean Fisher <srtfisher@gmail.com>
2013-08-24 13:42:06 -0400

    Going to be adding in unit testing from WP Core soon

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,18 +7,30 @@
  *
  * @package lockdown-wpadmin
  */
-if (! class_exists('PHPUnit_Framework_TestCase')) :
-	if (! file_exists(dirname(dirname(__FILE__)).'/vendor/autoload.php'))
-		die('Composer not initialized (PHPUnit not installed)');
-	else
-		require_once dirname(dirname(__FILE__)).'/vendor/autoload.php';
+if ( getenv( 'WP_TESTS_DIR' ) ) :
+	require_once getenv( 'WP_TESTS_DIR' ) . '/includes/functions.php';
+
+	function _manually_load_plugin() {
+		require dirname( __FILE__ ) . '/../sample-plugin.php';
+	}
+	tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
+
+	require getenv( 'WP_TESTS_DIR' ) . '/includes/bootstrap.php';
+else :
+	// Default back to local testing
+	if (! class_exists('PHPUnit_Framework_TestCase')) :
+		if (! file_exists(dirname(dirname(__FILE__)).'/vendor/autoload.php'))
+			die('Composer not initialized (PHPUnit not installed)');
+		else
+			require_once dirname(dirname(__FILE__)).'/vendor/autoload.php';
+	endif;
+
+	// Still dependant upon loading WP Core
+	require(dirname(__FILE__).'/../../../../wp-load.php');
+
+	error_reporting(-1);
+	ini_set('display_errors', 'on');
+	$_SERVER['HTTP_HOST'] = 'localhost';
+
+	require_once dirname(__FILE__).'/../lockdown-wp-admin.php';
 endif;
-
-// Still dependant upon loading WP Core
-require(dirname(__FILE__).'/../../../../wp-load.php');
-
-error_reporting(-1);
-ini_set('display_errors', 'on');
-$_SERVER['HTTP_HOST'] = 'localhost';
-
-require_once dirname(__FILE__).'/../lockdown-wp-admin.php';
